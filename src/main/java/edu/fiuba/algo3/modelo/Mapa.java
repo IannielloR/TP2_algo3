16f1122c54beda288eb96740e42d5b02b7a339b2
Tomás <iglesiastomas99@gmail.com>
2022-06-09 21:34:34 -0300

    Agrego comentarios hablados

--- src/main/java/edu/fiuba/algo3/modelo/Mapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/Mapa.java
@@ -7,6 +7,9 @@ public class Mapa {
     private int yVehiculo;
     private int xPozo;
     private int yPozo;
+
+    //private Moto moto;
+    //private Pozo pozo;
     public Mapa(){
         //Cantidad de esquinas que tiene el mapa
         this.xMaximo = 10;
@@ -15,6 +18,7 @@ public class Mapa {
         //Hay un pozo en 2,2
         this.xPozo = 2;
         this.yPozo = 2;
+
         //Vehiculo empieza en 1,1
         this.xVehiculo = 1;
         this.yVehiculo = 1;
@@ -23,6 +27,8 @@ public class Mapa {
     public int moverVehiculoDerecha(){
         xVehiculo++;
         if ((xVehiculo == xPozo) && (yVehiculo == yPozo)){
+            //bool motoPozo = moto.atravesoUnPozo();
+            //return pozo.movimientos(motoPozo);
             return 4;
         }
         return 1;
