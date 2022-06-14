d56006572d850a7a3f0c6824e847e3fae237e84f
Tomás <iglesiastomas99@gmail.com>
2022-06-13 23:55:24 -0300

    unifico tests

--- src/main/test/edu/fiuba/algo3/tp2Tests/vehiculoTest.java
+++ src/main/test/edu/fiuba/algo3/tp2Tests/vehiculoTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VehiculoTest {
     @Test
-    public void test(){
+    public void motoSeDesplazaYSeEncuentraUnPozo(){
         // Arrange
         Juego gpsChallenge = new Juego("moto");
         int movimientosEsperados = 6;
@@ -17,5 +17,22 @@ public class VehiculoTest {
 
         // Assert
         assertEquals(movimientosEsperados, gpsChallenge.getMovimientos());
+    }
+
+    @Test
+    public void cuatroXCuatroSeCruzaConUnControlPolicial(){
+        // Arrange
+        Juego gpsChallenge = new Juego("4x4");
+        int movimientosEsperadosA = 6;
+        int movimientosEsperadosB = 3;
+
+        // Act
+        gpsChallenge.moverVehiculoArriba();
+        gpsChallenge.moverVehiculoDerecha();
+        gpsChallenge.moverVehiculoAbajo();
+
+        // Assert
+        assertEquals(movimientosEsperadosA, gpsChallenge.getMovimientos());
+        assertEquals(movimientosEsperadosB, gpsChallenge.getMovimientos());
     }
 }
