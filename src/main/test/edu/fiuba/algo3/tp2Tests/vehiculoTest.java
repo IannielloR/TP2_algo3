53910e109b8074ff1654cbb5e391268139b971d5
Tomás <iglesiastomas99@gmail.com>
2022-06-13 23:51:32 -0300

    delete clases viejas

--- src/main/test/edu/fiuba/algo3/tp2Tests/vehiculoTest.java
+++ src/main/test/edu/fiuba/algo3/tp2Tests/vehiculoTest.java
@@ -1,18 +1,9 @@
 package edu.fiuba.algo3.tp2Tests;
 
-/*
-    Una moto atraviesa la ciudad y se encuentra con un Pozo. Es penalizada en tres movimientos.
-
-    Pozos: Le suma 3 movimientos de penalización a autos y motos.
-    Para una 4x4 penaliza en 2 movimientos luego de atravesar 3 pozos.
- */
-
-import edu.fiuba.algo3.modelo.Juego;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-
-public class test0 {
+public class VehiculoTest {
     @Test
     public void test(){
         // Arrange
