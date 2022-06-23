615202bd563bf7ee676c7fccf8b374a7ed5ac3bd
IannielloR <rianniello@fi.uba.ar>
2022-06-23 20:58:04 -0300

    actualizacion

--- src/main/java/edu/fiuba/algo3/modelo/TP2Proyect/modelo/interferencia/obstaculo/ControlPolicial.java
+++ src/main/java/edu/fiuba/algo3/modelo/TP2Proyect/modelo/interferencia/obstaculo/ControlPolicial.java
@@ -11,11 +11,7 @@ public class ControlPolicial extends Interferencia {
     public ControlPolicial(int xInicial, int yInicial, int xFinal, int yFinal) {
         this.coordenada.asignarCoordenadas(xInicial,  yInicial, xFinal, yFinal);
     }
-<<<<<<< HEAD
-    public int analizarVehiculo(TipoVehiculo vehiculo, int xInicial, int yInicial, int xFinal, int yFinal, int movimientos){
-=======
     public boolean analizarVehiculo(Vehiculo vehiculo, int xInicial, int yInicial, int xFinal, int yFinal){
->>>>>>> 9633c478fc4560ef4a348a96ab06bb5869a16af0
         if(coordenada.hayColision(xInicial, yInicial, xFinal, yFinal)){
             Random random = new Random();
             float prob = random.nextFloat();
