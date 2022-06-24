d8ec92285e83ab537b43ef779928d3396b1196fe
“NicolasPuccar” <npuccar@fi.uba.ar>
2022-06-24 20:42:42 -0300

    modificaciones de clases para finalizar modelo

--- src/main/test/edu/fiuba/algo3/TP2ProyectTests/tests/TestPruebaMoto.java
+++ src/main/test/edu/fiuba/algo3/TP2ProyectTests/tests/TestPruebaMoto.java
@@ -4,17 +4,16 @@ import edu.fiuba.algo3.modelo.TP2Proyect.modelo.Juego;
 import edu.fiuba.algo3.modelo.TP2Proyect.modelo.interferencia.obstaculo.ControlPolicial;
 import edu.fiuba.algo3.modelo.TP2Proyect.modelo.interferencia.obstaculo.Piquete;
 import edu.fiuba.algo3.modelo.TP2Proyect.modelo.interferencia.obstaculo.Pozo;
-import edu.fiuba.algo3.modelo.TP2Proyect.modelo.vehiculo.Auto;
-import edu.fiuba.algo3.modelo.TP2Proyect.modelo.vehiculo.CuatroXCuatro;
 import edu.fiuba.algo3.modelo.TP2Proyect.modelo.interferencia.Interferencia;
+import edu.fiuba.algo3.modelo.TP2Proyect.modelo.vehiculo.Moto;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestPrueba {
+public class TestPruebaMoto {
     @Test
-    public void test(){
+    public void test1(){
         // Arrange
-        Auto vehiculo = new Auto();
+        Moto vehiculo = new Moto();
         Juego gpsChallenge = new Juego(vehiculo);
 
         Interferencia obstaculo1 = new Pozo(1,2,2,2);
@@ -33,17 +32,55 @@ public class TestPrueba {
         gpsChallenge.agregarInterferencia(3,obstaculo4);
         gpsChallenge.agregarInterferencia(4,obstaculo5);
 
-        int movimientosEsperados = 21;
+        int movimientosEsperados = 22;
 
         // Act
         gpsChallenge.moverVehiculoAbajo();//(1,2)
         gpsChallenge.moverVehiculoDerecha();//(2,2)
         gpsChallenge.moverVehiculoDerecha();//(3,2)
         gpsChallenge.moverVehiculoAbajo();//(3,3)
-        gpsChallenge.moverVehiculoDerecha();//(3,3) no avanza
+        gpsChallenge.moverVehiculoDerecha();//(3,3)
+        gpsChallenge.moverVehiculoAbajo(); // (4,3)
+        gpsChallenge.moverVehiculoDerecha(); //(5,3)
+        gpsChallenge.moverVehiculoArriba(); // (5,2)
+        gpsChallenge.moverVehiculoArriba(); // (5,1)
+        gpsChallenge.moverVehiculoIzquierda(); // (4,1)
+        gpsChallenge.moverVehiculoIzquierda(); // (3,1)
 
-        gpsChallenge.moverVehiculoArriba(); // (3,2)
-        gpsChallenge.moverVehiculoDerecha(); // (4,2)
+//
+        // Assert
+        assertEquals(movimientosEsperados, gpsChallenge.getMovimientos());
+    }
+    @Test
+    public void test2(){
+        // Arrange
+        Moto vehiculo = new Moto();
+        Juego gpsChallenge = new Juego(vehiculo);
+
+        Interferencia obstaculo1 = new Pozo(1,2,2,2);
+
+        Interferencia obstaculo2 = new Pozo(2,2, 3,2);
+
+        Interferencia obstaculo3 = new Pozo(5,2,5,3);
+
+        Interferencia obstaculo4 = new Piquete(3,3,4,3);
+
+        Interferencia obstaculo5 = new ControlPolicial(4,1,3,1, (float) 0.1);
+
+        gpsChallenge.agregarInterferencia(0,obstaculo1);
+        gpsChallenge.agregarInterferencia(1,obstaculo2);
+        gpsChallenge.agregarInterferencia(2,obstaculo3);
+        gpsChallenge.agregarInterferencia(3,obstaculo4);
+        gpsChallenge.agregarInterferencia(4,obstaculo5);
+
+        int movimientosEsperados = 25;
+
+        // Act
+        gpsChallenge.moverVehiculoAbajo();//(1,2)
+        gpsChallenge.moverVehiculoDerecha();//(2,2)
+        gpsChallenge.moverVehiculoDerecha();//(3,2)
+        gpsChallenge.moverVehiculoAbajo();//(3,3)
+        gpsChallenge.moverVehiculoDerecha();//(3,3)
         gpsChallenge.moverVehiculoAbajo(); // (4,3)
         gpsChallenge.moverVehiculoDerecha(); //(5,3)
         gpsChallenge.moverVehiculoArriba(); // (5,2)
