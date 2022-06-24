5be7f7fc736822999384dbb3bd299bbdf26d7f7d
“NicolasPuccar” <npuccar@fi.uba.ar>
2022-06-24 19:04:08 -0300

    copiado de rama

--- src/main/java/edu/fiuba/algo3/modelo/TP2Proyect/modelo/Mapa.java
+++ src/main/java/edu/fiuba/algo3/modelo/TP2Proyect/modelo/Mapa.java
@@ -68,33 +68,11 @@ public class Mapa {
             vehiculoAvanza = interferencias.get(i).analizarVehiculo(this.vehiculo,posVehiculoX, posVehiculoY, posX, posY);
         }
 
-<<<<<<< HEAD
-        for(int i = 0; i < this.sorpresas.size(); i++){
-            movimientos += sorpresas.get(i).analizarVehiculo(this.vehiculo,posX, posY, movimientosTotales);
-
-        }
-        return movimientos;
-    }
-    private void crearInterferencias(){
-        Interferencia obstaculo1 = new Pozo(2,1);
-        Interferencia obstaculo2 = new Pozo(2,2);
-        Interferencia obstaculo3 = new Pozo(3,2);
-        Interferencia obstaculo4 = new Piquete(4,2);
-        Interferencia obstaculo5 = new ControlPolicial(4,1);
-
-        Interferencia obstaculo6 = new SorpresaDesfavorable(4,5);
-        Interferencia obstaculo7 = new SorpresaFavorable(3,3);
-        Interferencia obstaculo8 = new SorpesaCambioVehiculo(6,5);
-
-        Interferencia obstaculo9 = new Piquete(7,4);
-=======
         return vehiculoAvanza;
     }
->>>>>>> develop
 
 
     public void agregarInterferenciaAMapa(int posicion, Interferencia interferencia){
         this.interferencias.add(posicion,interferencia);
     }
 }
-
